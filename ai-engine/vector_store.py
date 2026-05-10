263b6b8533f5f8898f064c372bac8f51f5449756
rishabh1308 <rishabhsri1308@gmail.com>
2026-05-10 20:32:06 +0530

    some more changes

--- ai-engine/vector_store.py
+++ ai-engine/vector_store.py
@@ -1,18 +1,14 @@
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 
-print("STEP 1")
+
 
 embedding = OpenAIEmbeddings(
     model="text-embedding-3-small"
 )
-
-print("STEP 2")
 
 vector_DB = Chroma(
     collection_name="financial_memory",
     embedding_function=embedding,
     persist_directory="/tmp/chroma_db"
 )
-
-print("STEP 3")
