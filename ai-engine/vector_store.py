64ac50ce900e16a9b97484b2013a300e76ffcb96
rishabh1308 <rishabhsri1308@gmail.com>
2026-05-10 20:20:31 +0530

    some more changes

--- ai-engine/vector_store.py
+++ ai-engine/vector_store.py
@@ -1,9 +1,13 @@
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 
+print("STEP 1")
+
 embedding = OpenAIEmbeddings(
     model="text-embedding-3-small"
 )
+
+print("STEP 2")
 
 vector_DB = Chroma(
     collection_name="financial_memory",
@@ -11,14 +15,4 @@ vector_DB = Chroma(
     persist_directory="/tmp/chroma_db"
 )
 
-'''
-semantic memory of the User current and past profiles 
-it stores and retrieves embeddings 
-
-HuggingFace embedding converts txt -> numeric vector
-
-Chroma (Vector DB) stores Vector + Original text
-
-without this AI will become forgetful and stateless and will respond ,
-as per the current and not previous choices
-'''
+print("STEP 3")
